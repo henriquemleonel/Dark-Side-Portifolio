bcff8db39296c4b5d6b707c4daa485bb4d68530b
Henrique Marcio Leonel <henriquemleonel@outlook.com>
2020-02-03 23:29:24 -0300

    finalizando homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <body>
     <div class="wrapper">
-      <!-- navbar start here -->
+      <!--------------------- navbar start here ---------------------------------->
       <nav>
         <span id="brand">
-          <router-link id="a" to="/">rick__</router-link>
+          <router-link id="a" to="/">henriquemleonel <span class="dot">.</span></router-link>
           <!-- <a href="home">rick__</a> -->
         </span>
 
@@ -18,27 +18,27 @@
           </li>
         </ul>
 
-        <div id="toggle">
-          <div class="span">menu</div>
+        <div id="toggle" v-on:click="showMenu">
+          <div class="span-toggle">menu <span class="dot">.</span></div>
         </div>
       </nav>
 
-      <div class="resize">
-        <div class="close-btn"></div>
+      <div class="resize" :class="{ 'active' : varShowMenu }">
+        <div class="close-btn" v-on:click="showMenu">close <span class="dot">.</span></div>
 
         <ul id="menu">
-          <li><router-link class="a-resize-menu" to="/">Home</router-link> <span>.</span></li>
-          <li><router-link class="a-resize-menu" to="/about">About</router-link> <span>.</span></li>
-          <li><router-link class="a-resize-menu" to="/work">Work</router-link> <span>.</span></li>
+          <li><router-link class="a-resize-menu" to="/">home</router-link> <span>.</span></li>
+          <li><router-link class="a-resize-menu" to="/about">about</router-link> <span>.</span></li>
+          <li><router-link class="a-resize-menu" to="/work">work</router-link> <span>.</span></li>
           <li>
-            <router-link class="a-resize-menu" to="/contact">Contact</router-link> <span>.</span>
+            <router-link class="a-resize-menu" to="/contact">contact</router-link> <span>.</span>
           </li>
         </ul>
       </div>
 
-      <!-- navbar start here -->
+      <!--------------------------------------------------- navbar ends here -->
 
-      <!-- hero section start here -->
+      <!--------------------- hero section start here --------------------------->
       <div class="hero">
         <div class="header">
           <h1 class="line anim-typewriter">I make brands matter in culture</h1>
@@ -46,13 +46,13 @@
       </div>
 
       <div class="scroll-down"></div>
-      <!-- hero section ends here -->
+      <!------------------------------------------------ hero section ends here -->
 
       <!--------------------- project section start here ----------------------->
       <div class="container-fluid">
         <br /><br /><br />
 
-        <h6>selected projects</h6>
+        <h6>Yeah! Touch, this is art.</h6>
 
         <div class="vertical"></div>
         <br />
@@ -89,7 +89,7 @@
         <div class="row">
           <div class="col-lg-1"></div>
 
-          <div class="col-lg-5 project project5" @click="location.href='project.html'"></div>
+          <div class="col-lg-5 project project4" @click="location.href='project.html'"></div>
 
           <div class="col-lg-6"></div>
         </div>
@@ -107,7 +107,7 @@
           <div class="collab">
             <div class="row">
               <div class='col-lg-12'>
-                <p>Got a interesting project? I can help you.</p>
+                <p> Se interessou nos projetos? Eu posso te ajudar.</p>
               </div>
             </div>
           </div>
@@ -123,7 +123,7 @@
           <div class="info">
             <div class="row">
               <div class="col-lg-4" id="personal">
-                <p>connect with me</p>
+                <p>Conecte-se comigo</p>
                 <h4>ig @henriquemleonel</h4>
                 <br><br>
               </div>
@@ -142,7 +142,7 @@
               </div>
 
               <div class="col-lg-4" id="address">
-                <p>say hello</p>
+                <p>Say hello</p>
                 <h4>henriquemleonel@outlook.com</h4>
                 <br><br>
               </div>
@@ -160,14 +160,28 @@
   </body>
 </template>
 
+<script src="https://code.jquery.com/jquery-3.4.1.js"> </script>
+
 <script>
-// @ is an alias to /src
+
+// import gsap from 'gsap/all';
+// import { TweenMax, Expo } from 'gsap/all';
+// gsap.registerPlugin( TweenMax, Expo );
 
 export default {
   name: "home",
-  components: { }
+  components: { },
+  data() {
+    return {
+      varShowMenu: false,
+    }
+  },
+  methods: {
+    showMenu: function () {
+      this.varShowMenu = !this.varShowMenu;
+    }
+  }
 };
-
 // import { TweenMax } from 'gsap';
 </script>
 
@@ -175,10 +189,17 @@ export default {
 
 /* ------------------basics ----------------------- */
 body {
+  background: #0f0f0f !important;
+}
+
+.wrapper {
   margin: 0;
   padding: 0;
-  width: 100%;
+  width: 100vw;
+  overflow-y: hidden !important;
+  overflow-x: hidden;
   height: 100%;
+  width: 100%;
   font-family: "Roboto Mono" !important;
   background: #0f0f0f !important;
   color: #fff !important;
@@ -227,7 +248,7 @@ body {
       width: 0;
     }
     to {
-      width: 17em;
+      width: 14em;
     }
   }
 }
@@ -300,28 +321,34 @@ nav #menu li span {
   position: absolute;
   right: 30px;
   top: 20px;
-  font-weight: 300;
   z-index: 2;
-  width: 30px;
-  height: 30px;
+  width: 55px;
+  height: 35px;
   cursor: pointer;
   float: right;
   transition: all 0.3s ease-out;
   visibility: hidden;
   opacity: 0;
+  background-color: #0f0f0f;
+  border: none;
 }
 
 .close-btn {
   position: absolute;
   right: 16px;
+  min-width: 60px;
   font-weight: 300;
   z-index: 2;
   cursor: pointer;
   top: -2px;
   line-height: 80px;
+  //font-size: 22px;
+  color: #fff;
+  letter-spacing: 1px;
+  font-weight: 300;
 }
 
-#resize { 
+.resize { 
   z-index: 1;
   top: 0px;
   position: fixed;
@@ -330,36 +357,357 @@ nav #menu li span {
   height: 100%;
   visibility: hidden;
   opacity: 0;
-  transition: all 1s ease-out;
+  transition: all 0.8s ease-out;
 }
 
-#resize #menu {
+.resize #menu {
   height: 90px;
   position: absolute;
-  left: 45%;
-  transform: translateX(-40%);
+  left: 50%;
+  transform: translateX(-50%);
   text-align: center;
   display: table-cell;
   vertical-align: center;
 }
 
-#resize #menu li {
+.resize #menu li {
   display: block;
   text-align: center;
-  padding: 10px 0;
-  font-size: 50px;
-  min-height: 50px;
-  font-weight: bold;
+  padding: 20px 0;
+  font-size: 25px;
+  min-height: 60px;
+  font-weight: normal;
   cursor: pointer;
   transition: all 0.3s ease-out;
 }
 
-#resize li:nth-child(1) {
-  margin-top: 140px;
+.resize li:nth-child(1) {
+  margin-top: 180px;
 }
 
 .a-resize-menu {
   color: #fff;
+  letter-spacing: 2px;
+}
+
+.a-resize-menu:hover {
+  text-decoration: none;
+  color: aqua;
+}
+
+// .resize:
+.active {
+  visibility: visible;
+  opacity: 1;
+  transition: all 0.5s ease-out;
+}
+
+.dot {
+    font-weight: 700;
+  }
+
+@media(max-width: 768px) {
+  #a {
+    color: #fff;
+    transition: all 0.3s ease-out;
+    // font-size: 18px;
+    letter-spacing: 1px;
+  }
+
+  #toggle {
+    visibility: visible;
+    opacity: 1;
+    margin-top: 6px;
+    margin-right: 4px;
+    width: 48px;
+  }
+
+  .span-toggle {
+    color: #fff;
+    //font-size: 22px;
+    align-self: flex-end;
+    letter-spacing: 0px;
+    font-weight: 700;
+  }
+
+  nav #brand {
+    margin-left: 24px;
+  }
+
+  .a-menu {
+    font-size: 22px;
+    font-weight: 300;
+  }
+
+  .resize li span {
+    font-weight: bold;
+  }
+
+  nav #menu {
+    display: none;
+  }
+
+}
+
+@media(min-width: 769px) {
+  .resize {
+    visibility: hidden !important;
+  }
+}
+
+// -------------------- hero section --------------------------
+.hero { 
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header h1 {
+  font-weight: lighter;
+  text-align: center;
+  letter-spacing: -2px;
+  line-height: 58px;
+}
+
+@media(max-width: 768px) {
+  .hero {
+    overflow-x: hidden;
+  }
+  .header h1 {
+    font-size: 23px;
+    letter-spacing: 0px;
+  }
+}
+
+/** ---------------------- scroll down animation   ---------------------- */
+.scroll-down {
+  position: absolute;
+  left: 50%;
+  bottom: 100px;
+  display: block;
+  text-align: center;
+  font-size: 20px;
+  z-index: 0;
+  text-decoration: none;
+  text-shadow: 0;
+  width: 13px;
+  height: 13px;
+  border-bottom: 2px solid #fff;
+  border-right: 2px solid #fff;
+  transform: translate(-50%, 0) rotate(45deg);
+  animation: fade_move-down 3s cubic-bezier(0.19, 1, .22, 1) infinite;
+}
+
+@keyframes fade_move-down {
+  0% {
+    transform: translate(0, -20px) rotate(45deg);
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    transform: translate(0, 20px) rotate(45deg);
+    opacity: 0;
+  }
+}
+
+/** --------------------------------- projects section --------------------------------- */
+h6 {
+  text-align: center;
+}
+
+.vertical {
+  margin-top: 40px;
+  border-left: 1px solid #fff;
+  height: 80px;
+  position: absolute;
+  left: 50%;
+}
+
+.project { 
+  height: 500px;
+}
+
+.project1 {
+  background: url("../assets/images/dorso.jpg") no-repeat 50% 50%;
+  background-size: cover;
+}
+
+.project1::after {
+  content: "timeless flow.";
+  font-family: "Roboto Mono";
+  font-size: 36px;
+  position: relative;
+  color: #fff;
+  display: block;
+  top: 8%;
+  left: -70px;
+  right: 0;
+  bottom: 0;
+}
+
+.project2 {
+  background: url("../assets/images/woman.jpg") no-repeat 50% 50%;
+  background-size: cover;
+}
+
+.project2::after {
+  content: "foretime";
+  font-family: "Roboto Mono";
+  font-size: 36px;
+  position: relative;
+  color: #fff;
+  display: block;
+  top: 80%;
+  left: 90%;
+  right: 0;
+  bottom: 0;
+}
+
+.project3 {
+  background: url("../assets/images/man&woman.jpg") no-repeat 50% 50%;
+  background-size: cover;
+}
+
+.project3::after {
+  content: "never time";
+  font-family: "Roboto Mono";
+  font-size: 36px;
+  position: relative;
+  color: #fff;
+  display: block;
+  top: 80%;
+  left: -110px;
+  right: 0;
+  bottom: 0;
+}
+
+.project4 {
+  background: url("../assets/images/sculpture04.jpg") no-repeat 50% 50%;
+  background-size: cover;
+}
+
+.project4::after {
+  content: "this is the end";
+  font-family: "Roboto Mono";
+  font-size: 36px;
+  position: relative;
+  color: #fff;
+  display: block;
+  top: 80%;
+  left: -110px;
+  right: 0;
+  bottom: 0;
+}
+
+@media (max-width: 768px) {
+  .project {
+    width: 90% !important;
+    margin: 14px auto;
+  }
+
+  .vertical { display: none }
+
+  .project1::after, .project2::after, .project3::after, .project4::after {
+    display: none;
+  }
+}
+
+/**-------------------------------- footer section ------------------------ */
+.collab {
+  text-align: right;
+}
+
+.collab p {
+  font-weight: lighter !important;
+  margin-bottom: 20px;
+}
+
+.hr {
+  background: rgba(255, 255, 255, 0.2);
+  height: 1px;
+}
+
+.info h4 {
+  font-size: 18px;
+  font-weight: lighter;
+}
+
+.info p {
+  color: grey;
+  font-weight: lighter;
+}
+
+.info li {
+  font-weight: lighter;
+  color: #fff;
+  font-size: 16px;
+  padding-left: 20px;
+}
+
+#fb::before {
+  display: inline-block;
+  content: '';
+  border-radius: 100%;
+  height: 4px;
+  width: 4px;
+  margin-right: 6px;
+  background: #3b5998;
+}
+
+#ig::before {
+  display: inline-block;
+  content: '';
+  border-radius: 100%;
+  height: 4px;
+  width: 4px;
+  margin-right: 6px;
+  background: #fcaf45;
+}
+
+#tw::before {
+  display: inline-block;
+  content: '';
+  border-radius: 100%;
+  height: 4px;
+  width: 4px;
+  margin-right: 6px;
+  background: #55acee;
+}
+
+#yt::before {
+  display: inline-block;
+  content: '';
+  border-radius: 100%;
+  height: 4px;
+  width: 4px;
+  margin-right: 6px;
+  background: #c4302b;
+}
+
+#media, #address {
+  text-align: right;
+}
+
+#media ul {
+  list-style: none;
+}
+
+#media ul li {
+  display: inline-block;
+}
+
+@media(max-width: 768px) { 
+  .collab, #media, #personal, #address {
+    text-align: center;
+  }
+
+  .info ul {
+    margin: 0 0 0 -22px;
+    padding: 0;
+  }
 }
 
 </style>
